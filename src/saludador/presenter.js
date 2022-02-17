6cc576a4e47870e866eacc751ac3ed0b24864c6d
Israel Antezana Rojas <israelantezana@gmail.com>
2022-02-17 11:19:23 -0400

    Saluda por nombre

---
+++ src/saludador/presenter.js
@@ -0,0 +1,11 @@
+const nombre_input = document.querySelector("#nombre");
+const form = document.querySelector("#saludar-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = nombre_input.value;
+
+  div.innerHTML = "<p> Hola " + nombre + "</p>";
+});
